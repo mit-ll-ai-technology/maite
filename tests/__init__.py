ac0dbf3fb8596b85aeacb8068718f1ebd93d8675
Ryan Soklaski <ryan.soklaski@ll.mit.edu>
2023-01-12 16:54:29 -0500

    fix broken tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,7 +5,7 @@ from jatic_toolbox.testing.project import ModuleScan
 module_scan = ModuleScan()
 
 all_dummy_subpkgs = sorted(
-    p.name for p in (Path(__file__).parent / "dummy_projects").glob("*")
+    p.name for p in (Path(__file__).parent / "dummy_projects").glob("*") if p.is_dir()
 )
 
 assert all_dummy_subpkgs
