80aa7be72d0ecf82838e469ee4e1df713155a2cf
Garrett Botkin <garrett.botkin@ll.mit.edu>
2024-01-12 18:46:06 +0000

    268 remove torchmetrics limit

--- tests/test_interop/test_api_torchmetrics.py
+++ tests/test_interop/test_api_torchmetrics.py
@@ -5,15 +5,61 @@
 import pytest
 
 import maite
+from maite._internals.interop.registry import METRIC_REGISTRY
 
 METRICS = list(maite.list_metrics(provider="torchmetrics"))
+METRICS_WHITELIST = [
+    "Accuracy",
+    "AUROC",
+    "AveragePrecision",
+    "CalibrationError",
+    "CohenKappa",
+    "ConfusionMatrix",
+    "CosineSimilarity",
+    "ExactMatch",
+    "F1Score",
+    "FBetaScore",
+    "HammingDistance",
+    "HingeLoss",
+    "JaccardIndex",
+    "KLDivergence",
+    "MatthewsCorrCoef",
+    "MeanAbsoluteError",
+    "mean_average_precision",
+    "multiclass_accuracy",
+    "Precision",
+    "PrecisionRecallCurve",
+    "Recall",
+    "ROC",
+    "Specificity",
+    "StatScores",
+]
 
 
-@pytest.mark.parametrize("metric", METRICS + ["MeanAveragePrecision"])
+@pytest.mark.parametrize("metric", METRICS_WHITELIST)
 def test_api_torchmetrics_load_metric(metric):
-    m = maite.load_metric(
-        provider="torchmetrics", metric_name=metric, task="multiclass", num_classes=10
-    )
+    default_config = {
+        "provider": "torchmetrics",
+        "metric_name": metric,
+        "task": "multiclass",
+        "num_classes": 10,
+    }
+
+    if metric in METRIC_REGISTRY.keys():
+        config = METRIC_REGISTRY[metric]
+
+        if metric == "multiclass_accuracy":
+            config[
+                "num_classes"
+            ] = 10  # num_classes is a required value, needs to be supplied by user
+
+    elif metric in ["CosineSimilarity", "KLDivergence", "MeanAbsoluteError"]:
+        config = {"provider": "torchmetrics", "metric_name": metric}
+
+    else:
+        config = default_config
+
+    m = maite.load_metric(**config)
     assert m is not None
     assert hasattr(m, "update")
     assert hasattr(m, "compute")
