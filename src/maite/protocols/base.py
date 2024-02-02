34dd09f1bef74d94d96a8249eee927575b5f8e5c
Jeffrey Arena <jeffrey.arena@ll.mit.edu>
2024-02-02 13:35:17 -0500

    creating directory in which to store all protocol types and a templated base type that is templated on InputType, OutputType, MetadataType and their respective batched versions.

---
+++ src/maite/protocols/base.py
@@ -0,0 +1,17 @@
+# Copyright 2023, MASSACHUSETTS INSTITUTE OF TECHNOLOGY
+# Subject to FAR 52.227-11 – Patent Rights – Ownership by the Contractor (May 2014).
+# SPDX-License-Identifier: MIT
+
+__all__=[]
+
+
+from typing import TypeAlias, TypeVar
+
+InputType = TypeVar('InputType')
+OutputType = TypeVar('OutputType')
+DatumMetadataType = TypeVar('DatumMetadataType')
+
+InputBatchType = TypeVar('InputBatchType')
+OutputBatchType = TypeVar('OutputBatchType')
+DatumMetadataBatchType = TypeVar('DatumMetadataBatchType')
+
