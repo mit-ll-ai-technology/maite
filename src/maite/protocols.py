53efdfae35a449555608c51a4505b4f11d4c250a
Jeffrey Arena <jeffrey.arena@ll.mit.edu>
2024-02-02 13:36:15 -0500

    removing old protocols.py, the Generics on which protocols will be based are instead in maite/protocols/base.py

--- src/maite/protocols.py
+++
@@ -1,6 +0,0 @@
-# Copyright 2023, MASSACHUSETTS INSTITUTE OF TECHNOLOGY
-# Subject to FAR 52.227-11 – Patent Rights – Ownership by the Contractor (May 2014).
-# SPDX-License-Identifier: MIT
-
-__all__=[]
-
