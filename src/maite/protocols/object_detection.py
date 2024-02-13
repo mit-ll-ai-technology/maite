f49b26aa7569d08cb6945aa7c5fe90dbdf8d81ca
Jeffrey Arena <jeffrey.arena@ll.mit.edu>
2024-02-13 15:51:28 -0500

    Updating OutputType and OutputBatchType for object_detection problem to be an ordinary class object. Added copious notes in comments (some might graduate to issues/discussion before merging this code)

--- src/maite/protocols/object_detection.py
+++ src/maite/protocols/object_detection.py
@@ -5,9 +5,20 @@
 # import component generics from generic.py and specialize them for object detection
 # domain
 
-from typing import Protocol, Sequence, Any, runtime_checkable
+from typing import (
+    Protocol,
+    Sequence,
+    Any,
+    runtime_checkable,
+    TypedDict,
+    NamedTuple,
+    Hashable,
+    TypeAlias,
+    NewType,
+)
 
 import generic as gen
+
 
 @runtime_checkable
 class ArrayLike(Protocol):
@@ -15,8 +26,30 @@ class ArrayLike(Protocol):
         ...
 
 
+# We *could* make ArrayLike generic and rely on the subscripts for ArrayLike type
+# annotations to hint to the user the appropriate shape. No runtime safety would
+# be added by this approach because type subscripts are effectively invisible
+# at runtime. No additional static type-checking would occur either, but the
+# user would get useful type hints when cursoring over required inputs/outputs
+@runtime_checkable
+class ObjDetectionOutput(Protocol):
+    @property
+    def boxes(
+        self,
+    ) -> ArrayLike:  # shape [N, 4], format X0, Y0, X1, Y1 (document this somewhere?)
+        ...
+
+    @property
+    def labels(self) -> ArrayLike:  # label for each shape [N]
+        ...
+
+    @property
+    def scores(self) -> ArrayLike:  # shape [N]
+        ...
+
+
 InputType = ArrayLike
-OutputType = dict  # this should be some specialized dict, but for now make it trivial
+OutputType = ObjDetectionOutput
 MetadataType = object
 
 InputBatchType = ArrayLike
@@ -25,6 +58,7 @@ MetadataBatchType = Sequence[object]
 
 # TODO: Consider what pylance shows on cursoring over: "(type alias) Dataset: type[Dataset[ArrayLike, dict[Unknown, Unknown], object]]"
 # Can these type hints be made more intuitive? Perhaps given a name like type[Dataset[InputType = ArrayLike,...]]
+# - This will likely involve eliminating some TypeAlias uses.
 
 # TODO: Determine whether I should/can parameterize on the Datum TypeAlias.
 # This could make the pylance messages more intuitive?
@@ -34,6 +68,103 @@ MetadataBatchType = Sequence[object]
 #       ArrayLike in two separate method signatures, but the return type differs.
 #       In this case, typechecker seems to use the first matching method signature to
 #       determine type of output.
+
+# TODO: Consider potential strategies for defining OutputType
+#
+# Some potential solutions:
+## 0) A protocol class with named/typed fields like the following:
+##
+##      class ObjDetectionOutput(Protocol):
+##          x0: float
+##          y0: float
+##          x1: float
+##          x2: float
+##          label: int
+##          score: float
+##
+##    This has the following advantages:
+##       - Follows structural subtype class variance rules (ObjDetectionOutput covariant wrt read-only
+##         attributes, classes using ObjDetectionOutput as return type covariant wrt read-only attributes,
+##         and classes using ObjDetectionOutput as method argument type contravariant wrt
+##         read-only attributes.) (Similar to TypedDict)
+##       - Permits additional fields to be added by component implementors
+##         and application developers (unlike TypedDicts)
+##       - Permits implementor use in covariant contexts without either explicit importing
+##         or redefining protocols locally (i.e. in Dataset/Dataloader/Model components can
+##         return structural subtype of ObjDetectionOutput, but not in Augmentation or Metric
+##         components) (like TypedDicts, but doesn't cap additional fields)
+##       - Application developers could import implementor classes and workflows
+##         and be assured that all protocol-compliant workflows would interoperate.
+
+##
+## 1) A Typed Dict -- This type is self-documenting and would permit users to simply populate
+##    regular dictionaries in their implementations. The dictionary type is also
+##    familiar to users of TorchMetrics and TorchVision as an output type for object
+##    detection. Containing classes returning this object would be covariant
+##    in the types of the dictionary keys. Containing classes taking this type as an
+##    input argument would be contravariant wrt the types of the dictionary keys.
+##
+##    Using TypedDicts in this style ("as protocols", so to speak) does admit some challenges.
+##    Users would be unable to add any fields to their objects, and
+#
+# class ObjDetectionTypedDict(TypedDict)):
+#     x0: float
+#     x1: float
+#     y0: float
+#     y1: float
+#     label: int
+#     score: float
+#
+#     - For a user to pass static type checking in their own component or
+#       workflow implementations, we need to permit them to use this type
+#       as both an input (in Metric and Augmentation components) and an
+#       output (for Dataset, DataLoader, and Model components).
+#       Using a TypedDict as though it were a protocol has drawbacks
+#
+#       1) Import this particular TypedDict from MAITE
+#       2) Redefine a 'compatible' TypedDict
+#       3) "Under-annotate" to simply 'dict[str, Any]' and rely on MAITE utilities
+#          to complain about incompatible types
+#       4) Not annotate -- this is not a good answer
+#
+## 2) A typed 6-tuple -- this type clear about what the fields correspond to, but
+##    leverages a ubiquitous python class.
+#
+#     ObjDetectionOutput: TypeAlias = Tuple[float, float, float, float, int, float]
+#
+## 3) A named tuple -- This is very clear and using built-in Python, but this
+##    puts onus on implementor to return a named tuple since regular tuples with
+##    compatible sizes/types don't seem to support assignment to this named tuple type.
+#
+# class ObjDetectionNamedOutput(NamedTuple):
+#     x0: float
+#     x1: float
+#     y0: float
+#     y1: float
+#     label: int
+#     score: float
+#
+## 4) Variadic generics -- this type hint could specifies the purpose of
+##    each entry in a returned tuple, but using the type of each entry
+##    to denote its meaning masks potentially useful information about its
+##    real type. From the user's perspective, if a dimension is of type 'x0', what type is it?
+#
+##    The more obvious use case would be to denote size of Tensors/Arrays as below.
+##
+##    #An example of using type annotations to communicate shape information:
+##
+##    ```
+##    H: TypeAlias = int
+##    W: TypeAlias = int
+##    C: TypeAlias = int
+##
+##    def get_some_data() -> Tensor[H,W,C]:
+##    ...
+##    ```
+##
+##    This is also a relatively new language feature.
+##    See https://mit-ll-ai-technology.github.io/maite/explanation/type_hints_for_API_design.html#on-using-annotations-to-write-legible-documentation
+##    or PEP 646 for more information.
 
 Dataset = gen.Dataset[InputType, OutputType, MetadataType]
 DataLoader = gen.DataLoader[
@@ -62,7 +193,7 @@ Augmentation = gen.Augmentation[
 #
 # pretend we have a model such that:
 # InputType = np.array
-# OutputType = dict[str, Any] # should narrow this to make comparisons possible
+# OutputType = ObjDetectionOutput
 # MetadataType is an ordinary Python Class with integer-formatted 'id' field
 
 
@@ -73,15 +204,36 @@ from dataclasses import dataclass
 
 
 @dataclass
+class ObjDetectionOutput_impl:
+    boxes: np.ndarray = np.array(
+        [[0, 0, 1, 1], [1, 1, 2, 2]]
+    )  # shape [N, 4], format X0, Y0, X1, Y1 (document this somewhere?)
+    labels: np.ndarray = np.array([2, 5])  # shape [N]
+    scores: np.ndarray = np.array([0, 1])  # shape [N]
+
+    # This is a prescribed method to go from type required by protocol
+    # to the narrower implementor type
+    @classmethod
+    def from_arraylike(cls, odo: ObjDetectionOutput) -> "ObjDetectionOutput_impl":
+        return ObjDetectionOutput_impl(
+            boxes=np.array(odo.boxes),
+            labels = np.array(odo.labels),
+            scores=np.array(odo.scores),
+        )
+
+
+@dataclass
 class DatumMetadata_impl:
     id: int
 
+    # This is a prescribed method to go from type required by protocol
+    # to the narrower implementor type
     @classmethod
     def from_obj(cls, o: object) -> "DatumMetadata_impl":
         if isinstance(o, cls):
             return o
         else:
-            return DatumMetadata_impl(id=-1)  # just assign a random id
+            return DatumMetadata_impl(id=-1)  # just assign an id=1
 
 
 class DataSet_impl:
@@ -91,9 +243,11 @@ class DataSet_impl:
     def __len__(self) -> int:
         return 10
 
-    def __getitem__(self, h: Hashable) -> Tuple[np.ndarray, dict, DatumMetadata_impl]:
+    def __getitem__(
+        self, h: Hashable
+    ) -> Tuple[np.ndarray, ObjDetectionOutput_impl, DatumMetadata_impl]:
         input = np.arange(5 * 4 * 3).reshape(5, 4, 3)
-        output = dict(score=10)
+        output = ObjDetectionOutput_impl()
         metadata = DatumMetadata_impl(id=1)
 
         return (input, output, metadata)
@@ -103,9 +257,11 @@ class DataLoaderImpl:
     def __init__(self, d: Dataset):
         self._dataset = d
 
-    def __next__(self) -> Tuple[ArrayLike, list[dict], list[DatumMetadata_impl]]:
+    def __next__(
+        self,
+    ) -> Tuple[ArrayLike, list[ObjDetectionOutput_impl], list[DatumMetadata_impl]]:
         input_batch = np.array([self._dataset[i] for i in range(6)])
-        output_batch = [{"key": f"val{i}"} for i in range(6)]
+        output_batch = [ObjDetectionOutput_impl() for _ in range(6)]
         metadata_batch = [DatumMetadata_impl(i) for i in range(6)]
 
         return (input_batch, output_batch, metadata_batch)
@@ -121,13 +277,13 @@ class AugmentationImpl:
     @overload
     def __call__(
         self, _datum: Tuple[InputType, OutputType, MetadataType]
-    ) -> Tuple[np.ndarray, dict, DatumMetadata_impl]:
+    ) -> Tuple[np.ndarray, OutputType, DatumMetadata_impl]:
         ...
 
     @overload
     def __call__(
         self, _datum_batch: Tuple[InputBatchType, OutputBatchType, MetadataBatchType]
-    ) -> Tuple[np.ndarray, list[dict], list[DatumMetadata_impl]]:
+    ) -> Tuple[np.ndarray, OutputBatchType, list[DatumMetadata_impl]]:
         ...
 
     def __call__(
@@ -135,16 +291,17 @@ class AugmentationImpl:
         _datum_or_datum_batch: Tuple[InputType, OutputType, MetadataType]
         | Tuple[InputBatchType, OutputBatchType, MetadataBatchType],
     ) -> (
-        Tuple[np.ndarray, dict, DatumMetadata_impl]
-        | Tuple[np.ndarray, list[dict], list[DatumMetadata_impl]]
+        Tuple[np.ndarray, OutputType, DatumMetadata_impl]
+        | Tuple[np.ndarray, OutputBatchType, list[DatumMetadata_impl]]
     ):
         if isinstance(
             _datum_or_datum_batch[2], Sequence
         ):  # use second element's type to determine what input is
-            # process batch
+            # -- assume we are processing batch --
 
-            # type narrow (need to use functions like `isinstance`, `issubclass`, `type`, or user-defined typeguards)
-            # convert from broad types with guaranteed fields into specific types
+            # type narrow for static typechecker
+            # (For this need to use functions like `isinstance`, `issubclass`, `type`, or user-defined typeguards)
+            # We convert from broad types with guaranteed fields into specific types
 
             # Note: I'm not using parametrized information about generics because isinstance
             # checks don't apply to generics. But using the unparametrized generic is
@@ -159,10 +316,6 @@ class AugmentationImpl:
                 )  # Cant "isinstance check" against MetadataBatchType directly
             )
 
-            reveal_type(
-                _datum_or_datum_batch[1]
-            )  # Note: reveal_type with _datum_or_datum_batch object directly doesn't type-narrow
-
             input_batch_aug = np.array(_datum_or_datum_batch[0])
             output_batch_aug = [i for i in _datum_or_datum_batch[1]]
             metadata_batch_aug = [
@@ -174,7 +327,7 @@ class AugmentationImpl:
             return (input_batch_aug, output_batch_aug, metadata_batch_aug)
 
         else:
-            # process instance
+            # -- assume we are processing instance --
 
             assert (
                 isinstance(_datum_or_datum_batch[0], InputType)
@@ -190,6 +343,7 @@ class AugmentationImpl:
 
 
 class Model_impl:
+
     @overload
     def __call__(
         self, _input: InputType | InputBatchType
@@ -214,11 +368,11 @@ class Model_impl:
         if arr_input.ndim == 4:
             # process batch
 
-            return [dict(output=i) for i in range(arr_input.shape[0])]
+            return [ObjDetectionOutput_impl() for _ in range(10)]
 
         else:
             # process instance
-            return dict(output=0)
+            return ObjDetectionOutput_impl()
 
 
 class Metric_impl:
@@ -250,41 +404,49 @@ class Metric_impl:
 
 
 # try to run through "evaluate" workflow
-    
+
 aug: Augmentation = AugmentationImpl()
 metric: Metric = Metric_impl()
 dataset: Dataset = DataSet_impl()
 dataloader: DataLoader = DataLoaderImpl(d=dataset)
 model: Model = Model_impl()
 
-preds:list[OutputBatchType] = []
+preds: list[OutputBatchType] = []
 for input_batch, output_batch, metadata_batch in dataloader:
     input_batch_aug, output_batch_aug, metadata_batch_aug = aug(
         (input_batch, output_batch, metadata_batch)
     )
+    assert not isinstance(output_batch_aug, OutputType)
+    # This is onerous type-narrowing, because I can't run an isinstance check
+    # directly on parametrized generic types (e.g. 'list[OutputType]', which is 
+    # OutputBatchType). I have to use 'not isinstance' to rule out preds_batch
+    # being an OutputType instead.
 
     preds_batch = model(input_batch_aug)
-    
     assert not isinstance(preds_batch, OutputType)
+    #preds_batch = cast(OutputBatchType, preds_batch) # could do this instead
+
+    # Annoyingly, still need this type narrowing because type-checker can't
+    # predict the output type of Model.__call__ based on input type. (batch 
+    # input and singular inputs are both ArrayLike.) Perhaps we should explicitly
+    # require that the InputType be different than a InputBatchType
+
+    # The fact that InputType and InputBatchType are the same in this file 
+    # shows an interesting corner case for pyright. The static typechecker
+    # seems to take the first matching signature from Model_impl to determine
+    # the type of the returned variable. Thus, if multiple method overloads list
+    # the same input type and differing output types, only the first listed
+    # method will be considered. (Thus method ordering affects static type-correctness)
+    # This might be a reason to enforce InputType/InputBatchType to be different.
+
+    # Tracing TypeAliases is problematic for usability, but it can be more convenient
+    # for the developer to use them. The problem is that cursoring over an object
+    # with a TypeAliased type doesn't show the underlying type that would be 
+    # more meaningful to the user. "OutputBatchType" might a TypeAlias of
+    # "list[ObjDetectionOutput]" but the user can't figure that out without
+    # looking at source code. 
     preds.append(preds_batch)
-    # This is onerous type-narrowing, because I can't run an isinstance check
-    # directly on generic types (e.g. 'Sequence[dict]', which is OutputBatchType)
-    # I have to use 'not isinstance' to rule out preds_batch being a 
-    # dictionary instead of a sequence of dictionaries.
-    #
-    # Perhaps we should always make singular input/output/metadata types
-    # non-generic (parameterized or otherwise)?
 
     metric.update(preds_batch, output_batch_aug)
-    # problem: typechecker seems to assume that first matching signature
-    # in a set of 'overloads' happens to give the right return type. In
-    # the case of the Model component in Object detection problems, we
-    # want instances of Model component protocol to take ArrayLike objects
-    # for both batch and single inputs.
-
-    # another problem: output_batch_aug is a `list[dict]` not *literally*
-    # an OutputBatchType (a Sequence[dict]), only viewable after you trace)
-    # the TypeAliasing used in this file. FOr some reason, output_batch_aug
-    # is not interpreted as a valid implementor of Sequence[dict].
 
 metric_scores = metric.compute()
