e30cc4fbd5ca5a73b1466097c45ea78b059740dc
Jeffrey Arena <jeffrey.arena@ll.mit.edu>
2024-02-28 14:33:07 -0500

    populate maite.__version__ to pass tests/test_repo.test_version

--- src/maite/__init__.py
+++ src/maite/__init__.py
@@ -1,3 +1,5 @@
 # Copyright 2023, MASSACHUSETTS INSTITUTE OF TECHNOLOGY
 # Subject to FAR 52.227-11 – Patent Rights – Ownership by the Contractor (May 2014).
 # SPDX-License-Identifier: MIT
+
+from ._version import __version__
