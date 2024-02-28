11e6438679964b7309728edecc59c63ba1036b85
Jeffrey Arena <jeffrey.arena@ll.mit.edu>
2024-02-28 14:51:59 -0500

    Added back package-version-setting code to maite/__init__.py

--- src/maite/__init__.py
+++ src/maite/__init__.py
@@ -2,4 +2,12 @@
 # Subject to FAR 52.227-11 – Patent Rights – Ownership by the Contractor (May 2014).
 # SPDX-License-Identifier: MIT
 
-from ._version import __version__
+from typing import TYPE_CHECKING
+
+if not TYPE_CHECKING:
+    try:
+        from ._version import version as __version__
+    except ImportError:
+        __version__ = "unknown version"
+else:  # pragma: no cover
+    __version__: str
