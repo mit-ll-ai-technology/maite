229f764b32b3b1b299b0c70dfff8cf0fcff9a49a
Jeffrey Arena <jeffrey.arena@ll.mit.edu>
2024-03-20 17:32:56 -0400

    Updated some references to be 3.8 friendly, despite this stress test probably being blown away before merge to main.

--- stress_tests/image_classification_protocols.py
+++ stress_tests/image_classification_protocols.py
@@ -1,20 +1,20 @@
 import random
 from dataclasses import dataclass
-from typing import Any, Hashable, Sequence, Tuple, overload
+from typing import Any, Dict, Hashable, List, Sequence, Tuple, Union, overload
 
 import numpy as np
 import numpy.typing as npt
 
+from maite._internals.protocols import DatumMetadata
+from maite.protocols import ArrayLike
 from maite.protocols.image_classification import (
-    ArrayLike,
     Augmentation,
     DataLoader,
     Dataset,
-    DatumMetadata,
+    DatumMetadataBatchType,
+    DatumMetadataType,
     InputBatchType,
     InputType,
-    MetadataBatchType,
-    MetadataType,
     Metric,
     Model,
     TargetBatchType,
@@ -27,11 +27,11 @@ from maite.protocols.image_classification import (
 #
 # InputType = np.array of shape [H, W, C]
 # TargetType = np.array of shape [Cl]
-# MetadataType is an ordinary Python Class with integer-formatted 'id' field
+# DatumMetadataType is an ordinary Python Class with integer-formatted 'id' field
 #
 # InputBatchType = np.array of shape [N, H, W, C]
 # TargetBatchType = np.array of shape [N, Cl]
-# MetadataBatchType is an ordinary Python Class with integer-formatted 'id' field
+# DatumMetadataBatchType is an ordinary Python Class with integer-formatted 'id' field
 
 
 N_CLASSES = 5  # how many distinct classes are we predicting between?
@@ -94,7 +94,7 @@ class DataLoaderImpl:
 
     def __next__(
         self,
-    ) -> Tuple[ArrayLike, ArrayLike, list[DatumMetadata_impl]]:
+    ) -> Tuple[ArrayLike, ArrayLike, List[DatumMetadata_impl]]:
         input_batch = np.array([self._dataset[i] for i in range(6)])
         target_batch = np.array([self._dataset[i] for i in range(6)])
         metadata_batch = [DatumMetadata_impl(uuid=i) for i in range(6)]
@@ -111,26 +111,28 @@ class AugmentationImpl:
 
     @overload
     def __call__(
-        self, __datum: Tuple[InputType, TargetType, MetadataType]
+        self, __datum: Tuple[InputType, TargetType, DatumMetadataType]
     ) -> Tuple[npt.NDArray, npt.NDArray, DatumMetadata_impl]:
         ...
 
     @overload
     def __call__(
         self,
-        __datum_batch: Tuple[InputBatchType, TargetBatchType, MetadataBatchType],
-    ) -> Tuple[npt.NDArray, npt.NDArray, list[DatumMetadata_impl]]:
+        __datum_batch: Tuple[InputBatchType, TargetBatchType, DatumMetadataBatchType],
+    ) -> Tuple[npt.NDArray, npt.NDArray, List[DatumMetadata_impl]]:
         ...
 
     def __call__(
         self,
         _datum_or_datum_batch: (
-            Tuple[InputType, TargetType, MetadataType]
-            | Tuple[InputBatchType, TargetBatchType, MetadataBatchType]
+            Union[
+                Tuple[InputType, TargetType, DatumMetadataType],
+                Tuple[InputBatchType, TargetBatchType, DatumMetadataBatchType],
+            ]
         ),
     ) -> (
         Tuple[npt.NDArray, npt.NDArray, DatumMetadata_impl]
-        | Tuple[npt.NDArray, npt.NDArray, list[DatumMetadata_impl]]
+        | Tuple[npt.NDArray, npt.NDArray, List[DatumMetadata_impl]]
     ):
         if isinstance(
             _datum_or_datum_batch[-1], Sequence
@@ -168,7 +170,7 @@ class AugmentationImpl:
             assert (
                 isinstance(_datum_or_datum_batch[0], InputType)
                 and isinstance(_datum_or_datum_batch[1], TargetType)
-                and isinstance(_datum_or_datum_batch[2], MetadataType)
+                and isinstance(_datum_or_datum_batch[2], DatumMetadataType)
             )
 
             input_aug = np.array(_datum_or_datum_batch[0])
@@ -181,8 +183,8 @@ class AugmentationImpl:
 class Model_impl:
     @overload
     def __call__(
-        self, __input: InputType | InputBatchType
-    ) -> TargetType | TargetBatchType:
+        self, __input: Union[InputType, InputBatchType]
+    ) -> Union[TargetType, TargetBatchType]:
         ...
 
     @overload
@@ -195,8 +197,8 @@ class Model_impl:
 
     def __call__(
         self,
-        __input_or_input_batch: InputType | InputBatchType,
-    ) -> TargetType | TargetBatchType:
+        __input_or_input_batch: Union[InputType, InputBatchType],
+    ) -> Union[TargetType, TargetBatchType]:
         ...
 
         arr_input = np.array(__input_or_input_batch)
@@ -234,12 +236,12 @@ class Metric_impl:
 
     def update(
         self,
-        _preds: TargetType | TargetBatchType,
-        _targets: TargetType | TargetBatchType,
+        _preds: Union[TargetType, TargetBatchType],
+        _targets: Union[TargetType, TargetBatchType],
     ) -> None:
         return None
 
-    def compute(self) -> dict[str, Any]:
+    def compute(self) -> Dict[str, Any]:
         return {"metric1": "val1", "metric2": "val2"}
 
 
@@ -251,7 +253,7 @@ dataset: Dataset = Dataset_impl()
 dataloader: DataLoader = DataLoaderImpl(d=dataset)
 model: Model = Model_impl()
 
-preds: list[TargetBatchType] = []
+preds: List[TargetBatchType] = []
 for input_batch, target_batch, metadata_batch in dataloader:
     input_batch_aug, target_batch_aug, metadata_batch_aug = aug(
         (input_batch, target_batch, metadata_batch)
@@ -270,7 +272,7 @@ metric_scores = metric.compute()
 # Interesting "failure mode" for static type checking:
 # If you cursor over the type of metadata_batch that is returned from
 # "aug" function in evaluate workflow, you'll see it isn't a
-# list[DatumMetadata_impl] as you might expect. The Augmentation
+# List[DatumMetadata_impl] as you might expect. The Augmentation
 # implementation class has two methods that each take a tuple:
 # The first method signature is:
 #
@@ -278,12 +280,12 @@ metric_scores = metric.compute()
 #
 # and the second is:
 #
-#  (Tuple[ArrayLike, ArrayLike, list[object]]) -> Tuple[np.array, np.array, list[Augmentation_impl]]
+#  (Tuple[ArrayLike, ArrayLike, List[object]]) -> Tuple[np.array, np.array, List[Augmentation_impl]]
 #
-# So, given an input tuple with 3rd-element type 'list[object]' one might expect
-# the third element of the target tuple to be typed list[Augmentation_impl],
+# So, given an input tuple with 3rd-element type 'List[object]' one might expect
+# the third element of the target tuple to be typed List[Augmentation_impl],
 # but this is not the case. The reason is because everything in python is an
-# instance of type object (including list[object]). This is another example where
+# instance of type object (including List[object]). This is another example where
 # two compatible 'overload'ed type signatures cause the type-checker to use the
 # first and (quietly) ignore the second.
 #
